aec659c130fa9e771a8d25dd37f053a93d4de7f2
Aghogho Bernard <aghoghobernard@gmail.com>
2024-01-31 21:19:02 -0330

    Brevo Implementation

--- Saaly.Infrastructure/Configurations/MessagingConfig.cs
+++ Saaly.Infrastructure/Configurations/MessagingConfig.cs
@@ -3,6 +3,7 @@
     public class MessagingConfig : IConfig
     {
         public string? RegistrationQueue => ConfigHelper.Instance.GetValueFromAppSettings("Messaging:Queues:RegistrationQueue", defaultValue: "queue:registration-queue-dev");
+        public string? BrevoAPIKey => ConfigHelper.Instance.GetValueFromAppSettings("Messaging:Mailers:Brevo:APIKey", defaultValue: "");
         public void Validate()
         {
         }
--- Saaly.Infrastructure/Mailers/MailBag.cs
+++ Saaly.Infrastructure/Mailers/MailBag.cs
@@ -9,6 +9,9 @@ namespace Saaly.Infrastructure.Mailers
     public class MailBag
     {
         public string? Email { get; set; }
+        public string? HtmlBody { get; set; }    
         public string? Template { get; set; }
+        public string? Subject { get; set; }
+        public string? Sender { get; set; }
     }
 }
